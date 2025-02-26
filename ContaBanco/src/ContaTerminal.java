b6d1a42b67fc6b500a332f39803b9368fdda5acd
BorgesDineii <bdinei40@gmail.com>
2025-02-26 10:16:58 -0300

    mehorando o simulador

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -27,40 +27,55 @@ public class ContaTerminal {
         System.out.println("Olá " + nome +" "+ sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e esse é o saldo da sua conta R$" + saldo + ".");
         
         while (continuar) {
-            System.out.println("Gostaria de fazer um deposito? (s/n)");
-            String resposta = scanner.nextLine().toLowerCase();
+            //System.out.println("Gostaria de fazer um deposito? (s/n)");
+            //String resposta = scanner.nextLine().toLowerCase();
 
-            //System.out.println("\n Escolha uma opção: ");
-            //System.out.println("1 - Deposito");
-            //System.out.println("2 - Saque");
-            //System.out.println("3 - Saldo");
-            //System.out.println("4 - Sair");
-            //System.err.print("Opção: ");
-            //int opcao = scanner.nextInt();
+            System.out.println("\n Escolha uma opção: ");
+            System.out.println("1 - Deposito");
+            System.out.println("2 - Saque");
+            System.out.println("3 - Saldo");
+            System.out.println("4 - Sair");
+            System.err.print("Opção: ");
+            int opcao = scanner.nextInt();
             
-            if (resposta.equals("s")) {
+            if (opcao == 1) {
                 System.out.println("Por favor, insira o valor do depósito!");
                 double valorDeposito = scanner.nextDouble();
                 saldo += valorDeposito;
                 System.out.println("Seu depósito de R$ " + valorDeposito + " foi realizado com sucesso!");
                 System.out.println("Seu saldo é de R$" + saldo);
-            } else if (resposta.equals("n")) {
-                System.out.println("Nenhum depósito foi realizado.");
-                continuar = false;
-            } else {
-                System.out.println("Resposta invalida. Por favor, digite 's' para sim ou 'n' para não.");
-            }
-            System.out.println("Gostaria de fazer um saque? (s/n)");
-            String respost1 = scanner.nextLine().toLowerCase();
-            if (respost1.equals("s")) {
-                System.out.println("Por favor, insira o valor do saque:");
+            } else if (opcao == 2) {
+                System.out.println("Por favor insira o valor do saque");
                 double valorSaque = scanner.nextDouble();
-                saldo -= valorSaque;
-                System.out.println("Seu saque de R$ " + valorSaque + "foi realizado com sucesso!");
-                System.out.println("Seu saldo é de R$" + saldo);
+                if (valorSaque > 0 && valorSaque <= saldo) {
+                    saldo -= valorSaque;
+                    System.out.println("Saque de R$" + valorSaque + " realizado com sucesso!");
+                } else if (valorSaque > saldo){
+                    System.out.println("Sua conta possui o saldo de R$" + saldo +". Saldo insuficiente para saque! Por favor realize um depósito!");
+                } else {
+                    System.out.println("Valor invalido para saque.");
+                }
+                //continuar = false;
+            } else if (opcao == 3) {
+                System.out.println("O seu saldo atual é de R$" + saldo );
+            } else if (opcao == 4) {
+                continuar = false;
+                System.out.println("Obrigado por usar o simulador de banco!");
+            } else {
+                System.out.println("Opção invalida. Tente novamente.");
             }
+            //System.out.println("Gostaria de fazer um saque? (s/n)");
+            //String respost1 = scanner.nextLine().toLowerCase();
+            //if (respost1.equals("s")) {
+            //    System.out.println("Por favor, insira o valor do saque:");
+            //    double valorSaque = scanner.nextDouble();
+            //    saldo -= valorSaque;
+            //    System.out.println("Seu saque de R$ " + valorSaque + "foi realizado com sucesso!");
+            //    System.out.println("Seu saldo é de R$" + saldo);
+            //}
         }
-        System.out.println("Obrigado por usar o simulador de banco!");
+        scanner.close();
+        //System.out.println("Obrigado por usar o simulador de banco!");
     }
 }
 
