1d20c3e1d305dbbf8b1d9a800951914850cda090
BorgesDineii <bdinei40@gmail.com>
2025-04-08 10:06:24 -0300

    att

--- controle-candidatos/controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,14 +1,38 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        for(String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
         
         /*analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);/* */
 
+    }
+    static void entrandoEmContato(String Candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do {
+            atendeu= atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentantivasRealizadas++;
+            else
+                System.out.println("Contato realizado com sucesso");
+        }while(continuarTentando && tentativasRealizadas<3);
+        if(atendeu)
+            System.out.println("Conseguimos contato com" + candidato + " na " + tentativasRealizadas + " tentativa");
+        else System.out.println("Não conseguimos contato com " candidato + " número maximo de tentativas " + tentativasRealizadas);
+    }
+    // metodo auxiliar
+        static boolean atender() {
+            return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados() {
