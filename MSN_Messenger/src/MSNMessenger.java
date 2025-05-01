08c05407efb2e53c1cbce040cb5747af40afd4af
BorgesDineii <bdinei40@gmail.com>
2025-05-01 11:39:55 -0300

    att

---
+++ MSN_Messenger/src/MSNMessenger.java
@@ -0,0 +1,15 @@
+public class MSNMessenger {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem");
+ 
+    }
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+    public void validarConectadoInternet() {
+        System.out.println("Validando se está conectado a internet");
+    }
+    public void salvarHistoricoDeMensagem() {
+        System.out.println("Salvando historico de mensagem");
+    }
+}
