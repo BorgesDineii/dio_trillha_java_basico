2495bdba852f1187ad8199831f2707b565bc57e7
BorgesDineii <bdinei40@gmail.com>
2025-05-07 09:17:53 -0300

    poliformismo

--- Herança/src/ComputadorPedrinho.java
+++ Herança/src/ComputadorPedrinho.java
@@ -1,21 +1,40 @@
 public class ComputadorPedrinho {
     public static void main(String[] args) {
+        //Poliformismo
+        /* não sabe qual app 
+         * mas devera enviar e receber mensagem
+         */
+        ServicoDeMensagemInstantanea smi = null;
+
+        String appEscolhido = "msn";
+
+        if(appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
+        if(appEscolhido.equals("facebook"))
+            smi = new FacebookMessenger();
+        if(appEscolhido.equals("telegram"))
+            smi = new Telegram();
+
+        smi.enviarMensagem();
+        smi.receberMensagem();
+        
+
         //abrindo MSN Messenger
-        System.out.println("MSN");
-        MSNMessenger msn = new MSNMessenger();
-        msn.enviarMensagem();
-        msn.receberMensagem();
+        //System.out.println("MSN");
+        //MSNMessenger msn = new MSNMessenger();
+        //msn.enviarMensagem();
+        //msn.receberMensagem();
 
         //abrindo Facebook Messenger
-        System.out.println("Facebook");
-        FacebookMessenger fMessenger = new FacebookMessenger();
-        fMessenger.enviarMensagem();
-        fMessenger.receberMensagem();
+        //System.out.println("Facebook");
+        //FacebookMessenger fMessenger = new FacebookMessenger();
+        //fMessenger.enviarMensagem();
+        //fMessenger.receberMensagem();
 
         //abrindo telegram
-        System.out.println("Telegram");
-        Telegram telegram = new Telegram();
-        telegram.enviarMensagem();
-        telegram.receberMensagem();
+        //System.out.println("Telegram");
+        //Telegram telegram = new Telegram();
+        //telegram.enviarMensagem();
+        //telegram.receberMensagem();
     }
 }
