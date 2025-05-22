38480d42facd3765e368f309868b6477cb2d61da
BorgesDineii <bdinei40@gmail.com>
2025-05-22 11:03:57 -0300

    att

--- IInterfaces_funcionais/src/App.java
+++ IInterfaces_funcionais/src/App.java
@@ -1,5 +1,15 @@
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        public static void main(String[] args) {
+            List<User> users = List.of(new User(name:"Maria", age 21), new User(name:"João", age 32), new User   (name: "Eduado", age 40), new User(name"Ana", age 19));
+            //users.forEach(System.out::println);
+            printStringValue(Record::toString, users);
     }
+        private static void printStringValue(Function<User, String> callback, List<User> users) {
+            users.forEach(u -> System.out.println(callback.apply(u)));
+        }
 }
