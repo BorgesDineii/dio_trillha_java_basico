90a79efdf9d71d811ea96d7c63161593975cae1e
BorgesDineii <bdinei40@gmail.com>
2025-06-09 12:09:05 -0300

    add

--- operacao-Basica-Set/src/operacaoConvite/Convidado.java
+++ operacao-Basica-Set/src/operacaoConvite/Convidado.java
@@ -8,6 +8,28 @@ public class Convidado {
         return nome;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigoConvite;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Convidado other = (Convidado) obj;
+        if (codigoConvite != other.codigoConvite)
+            return false;
+        return true;
+    }
+
     public int getCodigoConvite() {
         return codigoConvite;
     }
